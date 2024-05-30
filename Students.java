a80780301ceac1d8edde9635d02b032cf42db8f8
flavia-sudo <kihahuflavia@gmail.com>
2024-05-30 18:47:06 +0300

    day

---
+++ Students.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+public class Students {
+    public static void main(String[] args){
+        String [] names = {"Joy, Jolly, Fred, John, James, Charles"};
+        System.out.println("Names:" + Arrays.toString(names));
+    }
+}
